14d5debf8197dab8fcd5332304c51517a584e8af
Jędrek <a.jwitas@gmail.com>
2018-11-22 23:24:04 +0100

    Dodaj pliki projektów.

---
+++ BethanyPieShop/Controlers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BethanyPieShop.Models;
+
+namespace BethanyPieShop.Controlers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Mesage = "Hello word";
+            return View();
+        }
+    }
+}
---
+++ BethanyPieShop/Models/MockPiesRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanyPieShop.Models
+{
+    public class MockPiesRepository : IPieRepository
+    {
+        private List<Pie> _pies;
+
+        public MockPiesRepository()
+        {
+            if (_pies == null)
+            {
+                InitializePies();
+            }
+
+        }
+
+
+
+        public IEnumerable<Pie> GetAllPies()
+        {
+            return _pies;
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return _pies.FirstOrDefault(p => p.Id == pieId);
+        }
+        private void InitializePies()
+        {
+            _pies = new List<Pie>
+            {
+            new Pie {Id = 1, Name = "Aple", ShortDescription ="ShortDescription", LongDescription = "LongDescription", Price = 10, ImageUrl = null, ImageThumbnailUrl = null, IsPieOfTheWeek = false},
+            new Pie {Id = 2, Name = "Aple", ShortDescription ="ShortDescription", LongDescription = "LongDescription", Price = 10, ImageUrl = null, ImageThumbnailUrl = null, IsPieOfTheWeek = false},
+            new Pie {Id = 3, Name = "Aple", ShortDescription ="ShortDescription", LongDescription = "LongDescription", Price = 10, ImageUrl = null, ImageThumbnailUrl = null, IsPieOfTheWeek = false}
+            };
+        }
+
+    }
+    
+}
